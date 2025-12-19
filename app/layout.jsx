91992328268b2f766727d4e74bb9bd6fedecdfdd
cusherah <cusherahkugan@gmail.com>
2025-12-19 21:13:55 +0530

    strated app files

---
+++ app/layout.jsx
@@ -0,0 +1,15 @@
+import '@/assets/styles/globals.css';
+const layout = ({children}) => {
+  return (
+    <html lang="en">
+        <body>
+        <div>
+        {children}
+    </div>
+        </body> 
+    </html>
+   
+  )
+}
+
+export default layout
