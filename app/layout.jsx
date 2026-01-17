0f22276c0f55ea0d50525e7e45a23d5764e79df4
cusherah <cusherahkugan@gmail.com>
2026-01-17 19:29:38 +0530

    fixed the version issue

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,22 @@
-import '@/assets/styles/globals.css';
-const layout = ({children}) => {
+import '@/assets/styles/globals.css'
+
+export const metadata = {
+  title: 'Property Lanka',
+  description: 'Real Estate Website',
+}
+
+const Mainlayout = ({children}) => {
   return (
     <html lang="en">
-        <body>
+       <body>
         <div>
-        {children}
-    </div>
-        </body> 
+      {children}
+       </div>
+       </body>
+ 
     </html>
    
   )
 }
 
-export default layout
+export default Mainlayout;
